62857b25426e0566b39b052977ae8084dfaff013
Tom Roelofs <tom12697@gmail.com>
2018-12-10 12:11:34 +0100

    Added base quizpage

--- Meating-App/meating_app/lib/quiz.dart
+++ Meating-App/meating_app/lib/quiz.dart
@@ -7,15 +7,50 @@ class QuizScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text("Quiz"),
       ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.pushReplacementNamed(
-                        context,
-                        'start',
-                      );
-          },
-          child: Text('Go back!'),
+      body: QuizForm(),
+    );
+  }
+}
+
+class QuizForm extends StatefulWidget {
+  @override
+  QuizFormState createState() => new QuizFormState();
+}
+
+class QuizFormState extends State<QuizForm> {
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          children: <Widget>[
+            SizedBox(height: 80.0),
+            Column(
+              children: <Widget>[
+                Text('Hoeveel procent van alle verkochte smartphones in 2017 had Android als OS? 87.7%', 
+                style: TextStyle(fontWeight: FontWeight.bold)),
+              ],
+            ),
+            SizedBox(height: 120.0),
+            ButtonBar(
+              alignment: MainAxisAlignment.center,
+              children: <Widget>[
+                FlatButton(
+                  child: Text('Previous'),
+                  onPressed: (){
+                    //show previous page
+                  },
+                ),
+                RaisedButton(
+                  child: Text('Next'),
+                  onPressed: (){
+                    //show next page
+                  },
+                ),
+              ],
+              )
+          ],
         ),
       ),
     );
