6c867cdb2f4c287f1d4cd516afbbc5ea0fc60aa4
sanderdl <sanderdlart@gmail.com>
2018-12-18 13:11:19 +0100

    Added padding back

--- Meating-App/meating_app/lib/quiz.dart
+++ Meating-App/meating_app/lib/quiz.dart
@@ -48,86 +48,89 @@ class QuizFormState extends State<QuizForm> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            SizedBox(height: 30.0),
-            Column(
-              children: <Widget>[
-                Text(questions[_position],
-                    style: Theme.of(context).textTheme.title),
-                SizedBox(height: 80.0),
-                SizedBox(
-                  child: GestureDetector(
-                    onPanUpdate: (details) {
-                      setState(() {
-                        RenderBox getBox = context.findRenderObject();
-                        panPos = getBox.globalToLocal(details.globalPosition);
-                        var center = new Offset(
-                            context.size.width / 2, context.size.height / 2);
-                        _percentage = calculatePercentage(panPos, center);
-                      });
-                    },
-                    behavior: HitTestBehavior.opaque,
-                    child: Stack(
-                        alignment: FractionalOffset.center,
-                        children: <Widget>[
-                          Positioned.fill(
-                            child: CustomPaint(
-                                foregroundPainter: PieChart(
-                                    fillColor: Colors.green[700],
-                                    backgroundColor: Colors.lightGreen,
-                                    percentage: _percentage)),
-                          ),
-                          Text(_percentage.round().toString() + '%',
-                              style:
-                                  TextStyle(fontSize: 25, color: Colors.white))
-                        ]),
+        child: Padding(
+          padding: EdgeInsets.all(24),
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 30.0),
+              Column(
+                children: <Widget>[
+                  Text(questions[_position],
+                      style: Theme.of(context).textTheme.title),
+                  SizedBox(height: 80.0),
+                  SizedBox(
+                    child: GestureDetector(
+                      onPanUpdate: (details) {
+                        setState(() {
+                          RenderBox getBox = context.findRenderObject();
+                          panPos = getBox.globalToLocal(details.globalPosition);
+                          var center = new Offset(
+                              context.size.width / 2, context.size.height / 2);
+                          _percentage = calculatePercentage(panPos, center);
+                        });
+                      },
+                      behavior: HitTestBehavior.opaque,
+                      child: Stack(
+                          alignment: FractionalOffset.center,
+                          children: <Widget>[
+                            Positioned.fill(
+                              child: CustomPaint(
+                                  foregroundPainter: PieChart(
+                                      fillColor: Colors.green[700],
+                                      backgroundColor: Colors.lightGreen,
+                                      percentage: _percentage)),
+                            ),
+                            Text(_percentage.round().toString() + '%',
+                                style: TextStyle(
+                                    fontSize: 25, color: Colors.white))
+                          ]),
+                    ),
+                    height: 200,
+                    width: 200,
                   ),
-                  height: 200,
-                  width: 200,
-                ),
-              ],
-            ),
-            SizedBox(height: 100.0),
-            ButtonBar(
-              alignment: MainAxisAlignment.center,
-              children: <Widget>[
-                RaisedButton(
-                  child: Text('Vorige'),
-                  onPressed: () {
-                    if (_position - 1 < 0) {
-                      return null;
-                    } else {
-                      setState(() {
-                        _position--;
-                        _percentage = 0;
-                      });
-                    }
-                  },
-                ),
-                RaisedButton(
-                  color: Theme.of(context).accentColor,
-                  textColor: Colors.white,
-                  splashColor: Colors.blueGrey,
-                  child: Text('Volgende'),
-                  onPressed: () {
-                    if (_position + 1 == questions.length) {
-                      Navigator.pushReplacementNamed(
-                        context,
-                        'highscore',
-                      );
-                    } else {
-                      setState(() {
-                        _position++;
-                        _percentage = 0;
-                        //send data to firebase
-                      });
-                    }
-                  },
-                ),
-              ],
-            )
-          ],
+                ],
+              ),
+              SizedBox(height: 100.0),
+              ButtonBar(
+                alignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  RaisedButton(
+                    child: Text('Vorige'),
+                    onPressed: () {
+                      if (_position - 1 < 0) {
+                        return null;
+                      } else {
+                        setState(() {
+                          _position--;
+                          _percentage = 0;
+                        });
+                      }
+                    },
+                  ),
+                  RaisedButton(
+                    color: Theme.of(context).accentColor,
+                    textColor: Colors.white,
+                    splashColor: Colors.blueGrey,
+                    child: Text('Volgende'),
+                    onPressed: () {
+                      if (_position + 1 == questions.length) {
+                        Navigator.pushReplacementNamed(
+                          context,
+                          'highscore',
+                        );
+                      } else {
+                        setState(() {
+                          _position++;
+                          _percentage = 0;
+                          //send data to firebase
+                        });
+                      }
+                    },
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
